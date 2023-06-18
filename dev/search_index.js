ec206c0295113b5c5792d7220568fcc8a4d0787c
Documenter.jl <documenter@juliadocs.github.io>
2023-06-18 09:21:33 +0000

    build based on 1398a77

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SuperLU_Base","category":"page"},{"location":"#SuperLU_Base","page":"Home","title":"SuperLU_Base","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SuperLU_Base.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SuperLU_Base]","category":"page"}]
+}

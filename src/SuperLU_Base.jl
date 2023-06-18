5b0a7ae0f56ee380db42074204ac80d5e76c730a
Will Kimmerer <kimmerer@mit.edu>
2023-06-18 03:44:48 -0400

    init from DIST

--- src/SuperLU_Base.jl
+++ src/SuperLU_Base.jl
@@ -1,5 +1,34 @@
 module SuperLU_Base
 
-# Write your package code here.
+using SparseBase
+# Common definitions for SuperLU, SuperLU_MT, and
+# SueprLU_DIST.
+include("common.jl")
+
+# convert and unsafe_convert definitions for formats.
+
+# NOTE: S *must* outlive the conversion.
+
+function Base.unsafe_convert(
+    ::Type{<:Common.NCformat}, S::SparseBase.CSCStore{Tv, CIndex{Ti}}
+) where {Tv, Ti}
+    return Common.NCformat{Ti}(
+        nstored(S), 
+        Ptr{Cvoid}(pointer(S.v)), 
+        Base.unsafe_convert(Ptr{Ti}, S.idx),
+        Base.unsafe_convert(Ptr{Ti}, S.ptr)
+    ), S
+end
+
+function Base.unsafe_convert(
+    ::Type{<:Common.NRformat}, S::SparseBase.CSRStore{Tv, CIndex{Ti}}
+) where {Tv, Ti}
+    return Common.NRFormat{Ti}(
+        nstored(S), 
+        Ptr{Cvoid}(pointer(S.v)), 
+        Base.unsafe_convert(Ptr{Ti}, S.idx),
+        Base.unsafe_convert(Ptr{Ti}, S.ptr)
+    ), S
+end
 
 end
